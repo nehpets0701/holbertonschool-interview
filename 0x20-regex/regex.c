b8b970960cb616c0c0679d7233389db452a3471e
Stephen <nehpets0701@gmail.com>
2022-03-23 22:36:20 -0400

    regex

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,12 @@
+#include "regex.h"
+
+/**
+ * regex_match-checks if regex matches string
+ * @str:string input
+ * @pattern:regex input
+ * Return: 0/1
+ */
+int regex_match(const char *str, const char *pattern)
+{
+	return (str && pattern);
+}
