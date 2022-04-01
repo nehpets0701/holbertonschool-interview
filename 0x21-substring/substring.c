150867322ce4b5894e23dfd8360ac7e21775bc56
Stephen <nehpets0701@gmail.com>
2022-03-31 23:23:55 -0400

    substring

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,19 @@
+#include "substring.h"
+
+
+/**
+ * find_substring - find all possible substrings
+ * @s: string
+ * @words: array of words
+ * @nb_words: number of elements
+ * @n: output
+ * Return: sunstring
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	(void)s;
+	(void)words;
+	(void)nb_words;
+	*n = 0;
+	return (NULL);
+}
